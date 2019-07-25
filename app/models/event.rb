2f8238ca50227233739efca96af8669bc29634d6
NeerNim <neernimt@gmail.com>
2019-07-25 16:57:25 +0200

    add validations for models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,9 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :attendances, foreign_key: :event_id
   has_many :attendees, through: :attendances
+  
+  validates :title, presence: true, length: {maximum: 50}
+  validates :description, presence: true, length: {maximum: 250}
+  validates :location, presence: true, length: {maximum: 150}
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
-  has_many :events, foreign_key: :creator_id
+  before_save { email.downcase! }
+  has_many :events, foreign_key: :creator_id, dependent: :destroy
   has_many :attendances, foreign_key: :attendee_id
   has_many :attended_events, through: :attendances, source: :event
 
-  
+  validates :name, presence: true, length: {maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, 
+                                    format: { with: VALID_EMAIL_REGEX},
+                                    uniqueness: {case_sensitive: false}
+
+
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
