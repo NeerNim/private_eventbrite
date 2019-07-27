43161920241f1e956eb5cded9152ab69ca63aaa9
NeerNim <neernimt@gmail.com>
2019-07-27 13:16:54 +0200

    add some datas and fix the future events in event show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,38 @@ User.create!(name:  "Genesis",
   name  = Faker::Name.name
   email = "genesis-#{n+1}@gmail.com"
   password = "password"
-  User.create!(name:  name,
+  creator = User.create!(name:  name,
                email: email,
                password:              password,
                password_confirmation: password)
+
+  Event.create!(creator_id: creator.id, 
+          title:"new party", 
+          date:Date.parse("Thur 23 Sep 2019"), 
+          time:"14:33:20", location:"home", 
+          description:"everyone is welcome")
+
+          Event.create!(creator_id: creator.id, 
+           title:"Tribute to 80's rock music", 
+          date:Date.parse("Thur 23 Sep 2015"), 
+          time:"14:33:20", location:"home", 
+          description:"come and celebrate 80's rock and roll music with us")
+        
 end
 
+event = Event.last
+
+User.limit(5).each do |user|
+  Attendance.create(event_id: event.id, attendee_id: user.id)
+end
+
+first = Event.first
+
+User.limit(8).each do |user|
+  Attendance.create(event_id: first.id, attendee_id: user.id)
+end
+
+
+
+
+ 
